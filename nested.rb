7cb6a52e12194fff7e852e0ffa7490714caad1dd
David Cha <david.cha44@qmail.cuny.edu>
2019-11-18 19:27:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,9 +65,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
-
-	programmer_hash =
+programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
